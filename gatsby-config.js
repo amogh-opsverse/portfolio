cd487f70c4a74f44d0b1085507e4926d03fbff50
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-03-06 21:27:11 -0800

    Initial Setup (#1)
    * add shared layout component
    
    * added mdx support
    
    * added sanity support

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.hera.org",
+    title: 'Portfolio Website',
+    description: 'Portfolio Website',
+    image: 'https://i.pinimg.com/236x/50/c2/e8/50c2e875992887a0814172c32782e189--ancient-greek-art-ancient-greece.jpg'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+
+    //This set of plugins is to enable creating MDX blog posts from the src/posts folder
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    'gatsby-remark-images',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+        defaultLayouts: {
+          posts: require.resolve('./src/components/post-layout.js')
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'vnkupgyb',
+        dataset: 'production',
+      }
+    }
+  ],
+
+};
---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../components/layout.js';
+
+export const query = graphql`
+  query GetImage {
+  file(name: {eq: "cocktail"}) {
+    childImageSharp {
+      gatsbyImageData(placeholder: TRACED_SVG)
+    }
+  }
+}
+`;
+
+export default function AboutPage({ data }) {
+  return (
+    <Layout
+      title="about this page"
+      description="more info"
+    >
+      <GatsbyImage
+        image={getImage(data.file)}
+        alt="image of a fancy drink"
+      />
+      <h1>About this site</h1>
+      <Link to="/">Back to home</Link>
+    </Layout>
+  )
+}
